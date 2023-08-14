9d938f0ba525c59b187636d83ae99a94b6e7d7c5
JollyR0ger <kav993@gmail.com>
2023-08-14 19:14:38 +0300

    finish contact us page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
 import ContactCard from '../components/ContactCard'
+import style from './page.module.scss'
 
-export default function Home() {
+export default function ContactUsPage() {
   return (
-    <div>
+    <div className={style.contactPage}>
+      <h1 className={style.title}>Contact Us</h1>
+      <p className={style.parag}>Any question or remarks? Just write us a message!</p>
       <ContactCard />
     </div>
   )
