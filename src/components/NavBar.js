d1373d8b5fae05e277eae603b186e61e519a3545
JollyR0ger <kav993@gmail.com>
2023-08-16 15:39:56 +0300

    add basic dropdown

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,47 @@
 import style from './navBar.module.scss'
 import Image from 'next/image'
 
+const DropdownLinks = ({ label, links }) => {
+  return (
+    <div className={style.dropdown}>
+      <p className={style.link}>{label}</p>
+      <ul className={style.dropdownList}>
+        {links.map((link, idx) => {
+          return (
+            <li key={idx}>
+              <Element {...link} />
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
 const Element = ({ label, link, links }) => {
+  if (links) {
+    return <DropdownLinks label={label} links={links} />
+  }
+
   return <a className={style.link} href={link}>{label}</a>
 }
 
 export default function NavBar({ navElements }) {
   return (
-    <nav className={style.navbar}>
+    <nav className={style.nav}>
       <Image
-        className={style.logo}
+        className={style.navLogo}
         src="/logo.svg"
         width={121}
         height={29}
         alt="Logo"
       />
       <div className={style.navLinks}>
-        <div className={style.elements}>
+        <div className={style.navElements}>
           {navElements?.map(el => <Element {...el} />)}
         </div>
-        <div className={style.userNav}>
-          <a className={style.icon} href="/profile">
+        <div className={style.navUser}>
+          <a className={style.navUserIcon} href="/profile">
             <Image
               src="/static/profile.svg"
               width={32}
@@ -28,7 +49,7 @@ export default function NavBar({ navElements }) {
               alt="Profile"
             />
           </a>
-          <a className={style.icon} href="/cart">
+          <a className={style.navUserIcon} href="/cart">
             <Image
               src="/static/cart.svg"
               width={32}
